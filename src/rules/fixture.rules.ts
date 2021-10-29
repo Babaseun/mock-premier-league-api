28142117c891f57c32e7cd55e1201c3d3a1487c2
Adeyemi Onibokun <ababaseun@gmail.com>
2021-10-29 04:49:52 +0100

    Added new rules to fixture rules

--- src/rules/fixture.rules.ts
+++ src/rules/fixture.rules.ts
@@ -5,21 +5,38 @@ export const fixtureRules = {
 	forAddFixture: [
 		check("homeTeam").not().isEmpty().withMessage("homeTeam field is required"),
 		check("awayTeam").not().isEmpty().withMessage("awayTeam field is required"),
-		check("matchInformation").not().isEmpty().withMessage("matchInformation field is required"),
-		check("kickoff").custom((kickoff) => {
+		check("homeTeam_score").optional().isNumeric().withMessage("homeTeam_score must be a number"),
+		check("awayTeam_score").optional().isNumeric().withMessage("awayTeam_score must be a number"),
+		check("status").optional().not().isEmpty().withMessage("awayTeam_score must be a number"),
+		check("kickoff").not().isEmpty().withMessage("kickoff field is required").custom((kickoff) => {
 			if (!moment(kickoff).isValid()) throw new Error("kickoff date provided is invalid");
 			if (!moment(kickoff).isAfter(new Date())) throw new Error("kickoff time needs to be in the future");
 			return true;
 		}),
+		check("stadium").not().isEmpty().withMessage("stadium field is required"),
 	],
 	forEditFixture: [
-		check("homeTeam").not().isEmpty().withMessage("homeTeam field is required"),
-		check("awayTeam").not().isEmpty().withMessage("awayTeam field is required"),
-		check("matchInformation").not().isEmpty().withMessage("matchInformation field is required"),
-		check("kickoff").custom((kickoff) => {
+		check("homeTeam").optional().not().isEmpty().withMessage("homeTeam field is required"),
+		check("awayTeam").optional().not().isEmpty().withMessage("awayTeam field is required"),
+		check("homeTeam_score").optional().isNumeric().withMessage("homeTeam_score must be a number"),
+		check("awayTeam_score").optional().isNumeric().withMessage("awayTeam_score must be a number"),
+		check("kickoff").optional().not().isEmpty().withMessage("kickoff field is required").custom((kickoff) => {
 			if (!moment(kickoff).isValid()) throw new Error("kickoff date provided is invalid");
 			if (!moment(kickoff).isAfter(new Date())) throw new Error("kickoff time needs to be in the future");
 			return true;
 		}),
+		check("stadium").optional().not().isEmpty().withMessage("stadium field is required"),
+		check("status").optional().not().isEmpty().withMessage("awayTeam_score must be a number"),
+	],
+	
+	forSearchFixture: [
+		check("name").optional().not().isEmpty().withMessage("name field is required"),
+		check("date").optional().not().isEmpty().withMessage("date field is required").custom((date) => {
+			if (!moment(date).isValid()) throw new Error("date provided is invalid");
+			return true;
+		}),
+		
+		check("stadium").optional().not().isEmpty().withMessage("stadium field is required"),
+		check("status").optional().not().isEmpty().withMessage("status must be a number"),
 	],
 };
