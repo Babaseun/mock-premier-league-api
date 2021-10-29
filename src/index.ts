a70ac43610fa014ff82d14261f14db26dd16e063
Adeyemi Onibokun <ababaseun@gmail.com>
2021-10-29 04:52:36 +0100

    Added rate limit

--- src/index.ts
+++ src/index.ts
@@ -13,50 +13,59 @@ import connectRedis from "connect-redis";
 import redis from "redis";
 import config from "./config/config";
 import http from "http";
+import rateLimit from "express-rate-limit";
+
+// limit IP requests
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
 
 const app = express();
 
 if (config.NODE_ENV !== "test") {
-	const RedisStore = connectRedis(session);
-	const redisClient = redis.createClient({
-		host: "localhost",
-		port: 6379,
-		password: "SUPER_SECRET_PASSWORD",
-	});
-	redisClient.on("error", (err) => {
-		console.log("Could not establish a connection with redis. " + err);
-	});
-	redisClient.on("connect", (err) => {
-		console.log("Connected to redis successfully");
-	});
-	
-	app.use(session({
-		store: new RedisStore({ client: redisClient }),
-		secret: "secret$%^134",
-		resave: false,
-		saveUninitialized: false,
-		cookie: {
-			secure: false, // if true only transmit cookie over https
-			httpOnly: false, // if true prevent client side JS from reading the cookie
-			maxAge: 1000 * 60 * 10, // session max age in miliseconds
-		},
-	}));
-	app.use(responseTime());
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient({
+    host: "redis",
+    port: 6379,
+  });
+  redisClient.on("error", (err) => {
+    console.log("Could not establish a connection with redis. " + err);
+  });
+  redisClient.on("connect", (err) => {
+    console.log("Connected to redis successfully");
+  });
+  app.use(rateLimit);
+  app.use(
+    session({
+      store: new RedisStore({ client: redisClient }),
+      secret: config.REDIS_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false, // if true only transmit cookie over https
+        httpOnly: false, // if true prevent client side JS from reading the cookie
+        maxAge: 1000 * 60 * 10, // session max age in miliseconds
+      },
+    })
+  );
+  app.use(responseTime());
 }
 const swaggerOptions = {
-	swaggerDefinition: {
-		info: {
-			version: "1.0.0",
-			title: "Mock Premier League API",
-			description: "An API that serves the latest scores of fixtures of matches in a Mock Premier League",
-			contact: {
-				name: "Adeyemi Onibokun",
-			},
-			servers: ["http://localhost:2000"],
-		},
-	},
-	
-	apis: ["src/index.ts", "src/routers/*.ts"],
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Mock Premier League API",
+      description:
+        "An API that serves the latest scores of fixtures of matches in a Mock Premier League",
+      contact: {
+        name: "Adeyemi Onibokun",
+      },
+      servers: ["http://localhost:5000"],
+    },
+  },
+
+  apis: ["src/index.ts", "src/routers/*.ts"],
 };
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
@@ -76,27 +85,26 @@ app.use(morgan("dev"));
  *        description: A successful response
  */
 app.get("/", (req, res) => {
-	return res.status(200).send({
-		message: "Welcome to Express API for mock premier league fixtures!",
-	});
+  return res.status(200).send({
+    message: "Welcome to Express API for mock premier league fixtures!",
+  });
 });
 
 app.use("/api/v1", userRouter);
 app.use("/api/v1", fixtureRouter);
 app.use("/api/v1", teamRouter);
 app.use("*", (req, res) => {
-	return res.status(404).send({
-		message: "API endpoint not found!",
-	});
+  return res.status(404).send({
+    message: "API endpoint not found!",
+  });
 });
 
 const PORT = 5000 || process.env.PORT;
 const server = http.createServer(app);
 if (config.NODE_ENV !== "test") {
-	server.listen(PORT,
-		async () => {
-			await connect();
-			console.log(`Server listening: http://localhost: ${PORT}`);
-		});
+  server.listen(PORT, async () => {
+    await connect();
+    console.log(`Server listening: http://localhost: ${PORT}`);
+  });
 }
 export default app;
